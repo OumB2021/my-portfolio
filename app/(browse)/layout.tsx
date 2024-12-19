e5590a9fd6c076cfcca62ed0619bcf7b9e629395
Oumb2021 <baarryoumar@gmail.com>
2024-12-19 16:22:40 -0500

    grain-bg

--- app/(browse)/layout.tsx
+++ app/(browse)/layout.tsx
@@ -1,11 +1,24 @@
 import Navbar from "@/components/navbar";
+import Image from "next/image";
 
 function BrowseLayout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="relative flex flex-col min-h-screen bg-gradient-to-tl from-zinc-50 to-zinc-100">
+    <div className="relative flex flex-col min-h-screen">
+      <div className="absolute inset-0 ">
+        <Image
+          src="https://img.freepik.com/premium-photo/hi-res-white-concrete-wall-texture_861973-352.jpg?w=1380"
+          alt="noise"
+          fill
+          style={{
+            objectFit: "fill",
+            opacity: 0.1,
+          }}
+        />
+      </div>
       <Navbar />
-      <div className="flex-1">{children}</div>
+      <div className="flex-1 z-10">{children}</div>
     </div>
   );
 }
+
 export default BrowseLayout;
